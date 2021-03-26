70a032801d456c7bf7562eb2360681671fa60a2c
Christopher Kim <chrisms91@gmail.com>
2021-03-26 01:50:16 -0700

    parse hashtag from the payload

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,22 +1,39 @@
+import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { FatText } from '../shared';
+import { Link } from 'react-router-dom';
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    font-weight: bold;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Comment = ({ author, payload }) => {
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: payload.replace(/#[\w]+/g, '<mark>$&</mark>'),
-        }}
-      />
+      <CommentCaption>
+        {payload.split(' ').map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{' '}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 };
